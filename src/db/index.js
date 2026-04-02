a0d7ebb8742b0d84896c98dd23233b64708ffa30
SK-knight <saksham.bds@gmail.com>
2026-04-02 21:31:49 +0530

    feat: add db.js in src/db and update index.js for database connection

---
+++ src/db/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { env } from "../config/env.js";
+
+let isConnected = false;
+
+
+const db = async () => {
+  console.log("Trying to connect to db...")
+  if (isConnected) {
+    console.log("Already connected to db ✅");
+    return;
+  }
+  await mongoose.connect(env.MONGO_URL, {
+    dbName: "auth_user",
+  });
+  isConnected = true;
+  console.log("Successfully connected to db... ✅");
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("🟢 DB connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("🔴 DB error: ", err);
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("🟡 DB disconnected");
+});
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import express from "express"
 
+import { env } from "./config/env.js"
+import db from "./db/index.js"
+
 const app = express()
 
-const port = 3000
-
+const port = env.PORT || 3000
+console.log(env)
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Task Manager 🙏')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const startServer = async()=>{
+  try{
+    await db()
+    app.listen(port,()=>{
+      console.log(`🚀 Server running on ${env.BASE_URL}`) 
+    })
+  }
+  catch(err){
+    console.log("☠️ Failed to connect to db.")
+    console.log("Error: ",err.message)
+    process.exit(1)
+  }
+}
+
+startServer()

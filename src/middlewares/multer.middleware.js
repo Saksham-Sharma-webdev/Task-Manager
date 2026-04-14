eb15761ae158bc511ab0c935032cf27d3d799ee0
SK-knight <saksham.bds@gmail.com>
2026-04-14 19:00:17 +0530

    feat: enhance multer middleware with validation and error handling

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import fs from "fs";
 import path from "path"
+import AppError from "../utils/app-error.js";
 
 const uploadPath = "./public/images";
 if (!fs.existsSync(uploadPath)) {
@@ -35,4 +36,24 @@ const upload = multer({
   },
 });
 
-export { upload };
+const uploadSingle = (fieldName) => {
+  return (req, res, next) => {
+    upload.single(fieldName)(req, res, (err) => {
+      if (err) {
+        if (err.code === "LIMIT_FILE_SIZE") {
+          return next(new AppError("File size exceeds 2MB", 400));
+        }
+
+        return next(new AppError(err.message, 400));
+      }
+
+      if (!req.file) {
+        return next(new AppError("Please upload an image", 400));
+      }
+
+      next();
+    });
+  };
+};
+
+export default uploadSingle

6d6e2f6a9401df97ee460e7cca54ab00a654a5bf
SK-knight <saksham.bds@gmail.com>
2026-05-13 13:26:06 +0530

    feat: add project routes

---
+++ src/routes/project.routes.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import { addMemberToProject, createProject, deleteMember, deleteProject, getProjectByid, getProjectMembers, getProjects, updateMemberRole, updateProject } from "../controllers/project.controller.js";
+import { isLoggedIn, validateProjectPermission } from "../middlewares/auth.middleware.js";
+import { AvailableUserRoles, UserRoleEnum } from "../constants/constants.js";
+import validate from "../middlewares/validator.middleware.js";
+import { addMemberToProjectValidator, updateProjectValidator } from "../validators/project.validators.js";
+
+const projectRouter = Router()
+
+projectRouter.use(isLoggedIn)
+
+projectRouter
+  .route("/")
+  .get(
+    getProjects
+  )
+  .post(
+    createProjectValidator(),
+    validate,
+    createProject
+  )
+
+projectRouter
+  .route("/:projectId")
+  .get(
+    validateProjectPermission(AvailableUserRoles),
+    getProjectByid
+  )
+  .patch(
+    validateProjectPermission([UserRoleEnum.ADMIN, UserRoleEnum.PROJECT_ADMIN]),
+    updateProjectValidator,
+    validate,
+    updateProject
+  )
+  .delete(deleteProject)
+
+projectRouter
+  .route("/:projectId/members")
+  .get(
+    validateProjectPermission(AvailableUserRoles),
+    getProjectMembers
+  )
+  .post(
+    validateProjectPermission([UserRoleEnum.ADMIN, UserRoleEnum.PROJECT_ADMIN]),
+    addMemberToProjectValidator(),
+    validate, 
+    addMemberToProject
+  )
+
+projectRouter
+  .route("/:projectId/m/:memberId")
+  .put(
+    validateProjectPermission([UserRoleEnum.ADMIN, UserRoleEnum.PROJECT_ADMIN]),
+    updateMemberRole
+  )
+  .delete(
+    validateProjectPermission([UserRoleEnum.ADMIN, UserRoleEnum.PROJECT_ADMIN]),
+    deleteMember
+  )
+
+export default projectRouter
